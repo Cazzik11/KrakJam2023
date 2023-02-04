4b345fc1903b8a9eacebb10e3311a856f7535e5a
Cazzik11 <75548194+Cazzik11@users.noreply.github.com>
2023-02-04 23:32:31 +0100

    Added muzzle for gun

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -5,11 +5,27 @@ using UnityEngine;
 public class Weapon : MonoBehaviour {
     public Transform firePoint;
     public GameObject bulletPrefab;
+    public Transform MuzzleTransform;
+    [Range(0f, 1f)]
+    public float CoolDown = 0.2f;
+
+    public GameObject MuzzleFlash;
+    public int FramesToFlash = 1;
+
+    float _nextFireTime = 0f;
+
+    bool _isFlashing = false;
+
+    void Start()
+    {
+        MuzzleFlash.SetActive(false);
+    }
 
     void Update() 
     {
         if (Input.GetButtonDown("Fire1"))
         {
+            _nextFireTime = Time.time + CoolDown;
             Shoot();
         }
     }
@@ -17,6 +33,27 @@ public class Weapon : MonoBehaviour {
     void Shoot ()
     {
        Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+       if (!_isFlashing)
+       {
+            StartCoroutine(DoFlash());
+       }
+
+    }
+
+    IEnumerator DoFlash()
+    {
+        MuzzleFlash.SetActive(true);
+        var framesFlashed = 0;
+        _isFlashing = true;
+
+        while(framesFlashed <= FramesToFlash)
+        {
+            framesFlashed++;
+            yield return null;
+        }
+
+        MuzzleFlash.SetActive(false);
+        _isFlashing = false;
     }
 }
 
