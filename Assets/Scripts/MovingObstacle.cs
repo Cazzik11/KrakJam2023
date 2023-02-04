493c525d60319fc67f49d27a941dedacf67d37bf
SPECjalny <specjalny99@gmail.com>
2023-02-04 04:41:42 +0100

    Added moving obstacle script

---
+++ Assets/Scripts/MovingObstacle.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Patrole : MonoBehaviour {
+
+    public float speed;
+    public float distance;
+
+    private bool movingRight = true;
+
+    public Transform groundDetection;
+
+    void Update(){
+    
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+
+        
+
+    }
+}
