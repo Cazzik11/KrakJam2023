81dfaeeb88d8491159872d83b1ed332f5bcc9338
Cazzik11 <75548194+Cazzik11@users.noreply.github.com>
2023-02-04 20:56:33 +0100

    Fixed killing enemies

---
+++ Assets/Scripts/EnemyDestroy.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDestroy : MonoBehaviour
+{
+    public GameObject EnemyDestroyEffect;
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.gameObject.tag == "Bullet")
+        {
+            Instantiate(EnemyDestroyEffect, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+    }
+
+}
